777b27c0d0a55340367a68c6482b3c323f91f845
ViniSilva32 <78766084+ViniSilva32@users.noreply.github.com>
2021-03-08 13:10:19 -0300

    Exercicios

---
+++ ExercicioUnity01IA/Assets/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Transform fim;
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (fim.position - transform.position).normalized * speed * Time.deltaTime;
+    }
+}
---
+++ ExercicioUnity01IA/Assets/Patrulha.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrulha : MonoBehaviour
+{
+    public List<Transform> pontos = new List<Transform>();
+    Transform fim;
+    int ponto = 0;
+    bool voltar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fim = pontos[ponto];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, fim.position) > 0.2f)
+            transform.position += (fim.position - transform.position).normalized * 8f * Time.deltaTime;
+        else
+            ChangePatrolPoint();
+    }
+    void ChangePatrolPoint()
+    {
+        if (!voltar)
+            ponto++;
+        else
+            ponto--;
+
+        if (ponto >= pontos.Count)
+        {
+            ponto = (pontos.Count - 1);
+            voltar = true;
+        }
+        else if (ponto < 0)
+        {
+            ponto = 0;
+            voltar = false;
+        }
+
+        fim = pontos[ponto];
+    }
+}
